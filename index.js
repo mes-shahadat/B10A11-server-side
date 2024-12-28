ae07940645b0fc352d5a1e684b37bfaa1e49fd32
mohammad yasin <binodonprojukti@gmail.com>
2024-12-28 17:37:31 +0600

    user POST route created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
+var cookieParser = require('cookie-parser')
 var cors = require('cors')
 require('dotenv').config()
 
@@ -7,9 +8,11 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors())
+app.use(cookieParser())
 app.use(express.json())
 
 const uri = process.env.DB_URI;
+const jwtSecret = process.env.JWT_SECRET
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,6 +28,7 @@ async function run() {
     try {
 
         const database = client.db("car_rental");
+        const allUser = database.collection("all users");
         const allCar = database.collection("all cars");
 
         // GET
@@ -34,6 +38,15 @@ async function run() {
         })
 
         // POST
+        app.post('/user', async (req, res) => {
+
+            const doc = req.body
+
+            const result = await allUser.insertOne(doc);
+
+            res.json(result)
+        })
+
         app.post("/car", async (req, res) => {
 
             const doc = req.body
